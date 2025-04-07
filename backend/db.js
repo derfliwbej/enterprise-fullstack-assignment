dcfd5f3e3efaae5f4f88fd7868e172bff7b88cba
Jeb Wilfred Panganiban <jwdpanganiban@gmail.com>
2025-04-07 12:20:46 +0800

    fix: host db

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // Create a connection pool to the PostgreSQL database
 const pgPool = new Pool({
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
