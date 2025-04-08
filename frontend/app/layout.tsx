40701a07555f4c3dd5d29a93f97889e5f2ba0f98
Jeb Wilfred Panganiban <jwdpanganiban@gmail.com>
2025-04-08 16:01:37 +0800

    chore: app metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,15 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { QueryProvider } from '@/lib/queryClient';
+import { Metadata } from 'next';
 import React from 'react';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Chartmetric Assignment',
+  authors: [
+    { name: 'Jeb Wilfred Panganiban', url: 'https://www.jwdpanganiban.com' },
+  ],
+};
 
 export default function RootLayout({
   children,
